e55449bb456ef2c579066ee4702a49a161e1ce8c
touros91 <andreageraci91@gmail.com>
2022-04-28 21:09:40 +0200

    Add History component

---
+++ src/components/History.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <div class="row">
+      <div class="col">
+        <h2>
+          History and <br />
+          Patriarcal season
+        </h2>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-12 col-lg-6 left">
+        <p>
+          On the other hand, we denounce with righteous indignation and dislike
+          men who are so beguiled and demoralized by the charms of pleasure of
+          the moment, so blinded by desire
+        </p>
+      </div>
+      <div class="col-xs-12 col-lg-6">
+        <p>
+          But I must explain to you how all this mistaken idea of denouncing
+          pleasure and praising pain was born and I will give you a complete
+          account of the system, and expound the actual teachings of the great
+          explorer of the truth, the master-builder of human happiness. No that
+          are extremely painful
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+div {
+  font-family: "Miller-Display";
+  padding: 20px 0;
+
+  h2 {
+    color: #111111;
+    font-size: 50px;
+    margin-bottom: 20px;
+  }
+
+  p {
+    color: #acacac;
+    font-size: 15px;
+    text-align: left;
+  }
+
+  .left {
+    padding-right: 120px;
+  }
+}
+</style>
