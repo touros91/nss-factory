697f53ff7e8261db943e3425f184a13b60d18c0e
touros91 <andreageraci91@gmail.com>
2022-04-28 21:11:05 +0200

    Add fonts, images and general refactoring

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,91 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <header>
+      <NavBar :navBar="navBar" />
+    </header>
+    <main>
+      <section>
+        <Hero />
+      </section>
+      <section class="container-fluid">
+        <div class="row">
+          <History />
+        </div>
+      </section>
+      <section class="container-fluid">
+        <div class="row">
+          <Collection title="Temporary Store" />
+          <ImageComponent source="assets/img/store-snow.png" />
+        </div>
+        <div class="row">
+          <ImageComponent source="assets/img/store.png" />
+          <Collection title="New Collection Henry London" />
+        </div>
+      </section>
+      <section>
+        <Press />
+      </section>
+    </main>
+  </div>
 </template>
 
 <script>
+import NavBar from "./components/NavBar";
+import Hero from "./components/Hero";
+import History from "./components/History";
+import Collection from "./components/Collection";
+import ImageComponent from "./components/ImageComponent";
+import Press from "./components/Press";
+
 export default {
   name: "App",
-  components: {},
+  components: {
+    NavBar,
+    Hero,
+    History,
+    Collection,
+    ImageComponent,
+    Press,
+  },
+  data() {
+    return {
+      navBar: [
+        "About us",
+        "Brand",
+        "Projects",
+        "Magazine",
+        "Press",
+        "Contacts",
+      ],
+    };
+  },
 };
 </script>
 
 <style lang="scss">
 @import "src/assets/style/common.scss";
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@font-face {
+  font-family: "Montserrat-Regular";
+  src: local("Montserrat"),
+    url(./assets/font/Montserrat-Regular.ttf) format("truetype");
+}
+@font-face {
+  font-family: "Montserrat-Bold";
+  src: local("Montserrat"),
+    url(./assets/font/Montserrat-Bold.ttf) format("truetype");
+}
+@font-face {
+  font-family: "Miller-Display";
+  src: local("Miller"),
+    url(./assets/font/Miller-Display_30501.ttf) format("truetype");
+}
+@font-face {
+  font-family: "Merriweather-Regular";
+  src: local("MontsMerriweathererrat"),
+    url(./assets/font/Merriweather-Regular.ttf) format("truetype");
+}
+
+.row {
+  padding: 0 50px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+// Bootstrap //
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+Vue.config.productionTip = false;
+
+// Fontawesome
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faMagnifyingGlass,
+  faAngleRight,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faMagnifyingGlass, faAngleRight);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
