858d7f755cf2c65bd62c47c3d3434657a02e4d7b
touros91 <andreageraci91@gmail.com>
2022-04-30 16:56:39 +0200

    Add ImageSmall component

---
+++ src/components/ImageSmall.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="col-xs-12 col-lg-6 mt-5 mt-lg-0">
+    <img :src="require(`@/${source}`)" alt="London Bus" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    source: {
+      type: String,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "src/assets/style/partials/variables.scss";
+
+div {
+  .col-lg-6 {
+    padding: 0;
+    img {
+      width: 100%;
+    }
+  }
+}
+</style>
