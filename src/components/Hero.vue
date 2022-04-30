6968c3a76a3da0605aed86c566cda8dd6c0ef8de
touros91 <andreageraci91@gmail.com>
2022-04-30 16:53:39 +0200

    Add carousel

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -9,122 +9,165 @@
       <p>An estimable experience in the modern collection house</p>
       <button>Discover</button>
     </div>
-    <div class="slider d-none d-lg-block">
-      <div class="slider-img">
-        <img
-          src="https://www.holzkern.com/media/holzkern/cache/1080/catalog/product/b/e/belvue1.2.webp"
-          alt="Watch"
-        />
-        <p class="p">The Path to Success with Watchlab</p>
-        <button>
-          NEXT
-          <font-awesome-icon class="arrow" icon="fa-solid fa-angle-right" />
-        </button>
+    <!-- slider -->
+    <div
+      id="carouselExampleControls"
+      class="carousel slide d-none d-lg-block"
+      data-bs-ride="carousel"
+    >
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img
+            :src="require(`@/assets/img/carousel1.webp`)"
+            class="d-block w-100"
+            alt="Woman Henry London Watch"
+          />
+        </div>
+        <div class="carousel-item">
+          <img
+            :src="require(`@/assets/img/carousel2.jpeg`)"
+            class="d-block w-100"
+            alt="Man Henry London Watch"
+          />
+        </div>
+        <div class="carousel-item">
+          <img
+            :src="require(`@/assets/img/carousel3.webp`)"
+            class="d-block w-100"
+            alt="Women Henry London Watch"
+          />
+        </div>
       </div>
+      <div class="carousel-text">
+        The Path to Success <br />
+        with Watchlab
+      </div>
+      <button
+        class="carousel-control-next"
+        type="button"
+        data-bs-target="#carouselExampleControls"
+        data-bs-slide="next"
+      >
+        <span class="">NEXT</span>
+        <font-awesome-icon icon="fas fa-angle-right" class="animation" />
+      </button>
     </div>
+    <!-- /slider -->
   </div>
 </template>
 
-<script>
-export default {};
-</script>
-
 <style lang="scss" scoped>
+@import "src/assets/style/partials/variables.scss";
+@import "src/assets/style/partials/mixins.scss";
+
 .hero {
   width: 100vw;
   height: 650px;
+  padding: 400px 0;
   position: relative;
-  background-image: url(../assets/img/office.jpeg);
+  background-image: url("../assets/img/office.jpg");
   object-fit: cover;
   background-position: center;
   background-size: cover;
   background-repeat: no-repeat;
   box-shadow: inset 0 0 0 1000px rgba(0, 0, 0, 0.4);
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  @include flex-row(center, center);
 
   .content {
     position: absolute;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
-    color: white;
+    color: $color-white;
     font-weight: 900;
     text-align: center;
-    font-family: "Montserrat-Bold";
+    font-family: $montserrat-bold;
 
     h6 {
       font-size: 14px;
     }
 
     h1 {
-      font-family: "Miller-Display";
+      font-family: $miller-display;
       font-size: 70px;
     }
 
     p {
       font-size: 12px;
-      font-family: "Montserrat-Regular";
+      font-family: $montserrat;
     }
 
     button {
       padding: 8px 70px;
-      border: 1px solid white;
-      color: white;
+      border: 1px solid $color-white;
+      color: $color-white;
       font-weight: 100px;
       background: transparent;
       cursor: pointer;
-      font-family: "Miller-Display";
+      font-family: $miller-display;
 
       &:hover {
-        background: #d0c3b6;
+        background: $color-beige;
       }
     }
   }
+}
 
-  .slider {
-    width: 250px;
-    position: absolute;
-    right: 50px;
-    top: 450px;
-    // position: relative;
+.carousel {
+  color: $color-white;
+  overflow: hidden;
+  width: 350px;
+  position: absolute;
+  bottom: 0;
+  right: 50px;
 
-    .slider-img {
-      position: relative;
-
-      img {
-        width: 100%;
-        height: 200px;
-        object-fit: cover;
-        opacity: 0.9;
-      }
-
-      p {
-        position: absolute;
-        left: 15px;
-        top: 25px;
-        color: white;
-        width: 200px;
-        font-family: "Miller-Display";
-        font-size: 20px;
-      }
-
-      button {
-        position: absolute;
-        right: 25px;
-        bottom: 25px;
-        border: none;
-        background: transparent;
-        color: white;
-        font-family: "Montserrat-Regular";
-        font-size: 15px;
-
-        .arrow {
-          margin-left: 15px;
-        }
-      }
+  .carousel-item {
+    img {
+      height: 230px;
+      object-fit: cover;
     }
+  }
+  .carousel-control-next {
+    opacity: 1;
+    top: 65%;
+    right: 50px;
+    font-family: $montserrat-bold;
+
+    .fa-angle-right {
+      margin-left: 20px;
+    }
+  }
+  .carousel-text {
+    position: absolute;
+    z-index: 1;
+    font-family: $miller-display;
+    top: 30px;
+    left: 20px;
+    font-size: 23px;
+  }
+  .carousel-inner::after {
+    content: "";
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    left: 0;
+    z-index: 1;
+    background-color: rgba(0, 0, 0, 0.2);
+    transition: all 0.5s;
+  }
+}
+
+.carousel:hover .animation {
+  animation: move 1s infinite;
+}
+@keyframes move {
+  from {
+    transform: translateX(0);
+    opacity: 0;
+  }
+  to {
+    transform: translateX(15px);
+    opacity: 1;
   }
 }
 </style>
